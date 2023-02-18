1bfd2920348538b42f64d3f00e4c41d8c7d6c7bd
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-18 08:28:19 +0000

    add commands.js file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,26 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import Home from "../pages/Home";
+import Search from "../pages/Search";
+
+const home = new Home();
+const search = new Search();
+
+Cypress.Commands.add("rejectCookies", () => {
+  cy.contains("Reject all").click();
+});
+
+Cypress.Commands.add("assertThatUserIsOnHomepage", () => {
+  home.assertThatUserIsOnHomepage();
+});
+
+Cypress.Commands.add("clickSearch", () => {
+  home.clickSearch();
+});
+
+Cypress.Commands.add("searchFor", (product) => {
+  home.searchFor(product);
+  search.assertSearch(product);
+});
+
+Cypress.Commands.add("addToCart", (nth) => {
+  search.addToCart(nth);
+});
