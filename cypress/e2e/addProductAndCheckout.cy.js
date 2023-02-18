b0cc80e1496a43d705e1db59643a49585513396b
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-18 08:28:52 +0000

    add test.cy.js file

--- cypress/e2e/addProductAndCheckout.cy.js
+++ cypress/e2e/addProductAndCheckout.cy.js
@@ -1,5 +1,15 @@
+/// <reference types="Cypress" />
+
 describe("Adding a product to the cart and completing the checkout process", () => {
-  it("", () => {
+  it("Visits site, searches for an item, and then proceeds to checkout", () => {
+    // Home
     cy.visit("/");
+    cy.rejectCookies();
+    cy.assertThatUserIsOnHomepage();
+    cy.clickSearch();
+    cy.searchFor("Hand Soap");
+
+    // Search
+    cy.addToCart(0);
   });
 });
