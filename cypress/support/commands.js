2450a62a5c6de571c06e7c2c20749edad32ec0c0
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-19 14:14:38 +0000

    update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,12 @@
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
 
 const home = new Home();
 const search = new Search();
 const cart = new Cart();
+const checkout = new Checkout();
 
 Cypress.Commands.add("rejectCookies", () => {
   cy.contains("Reject all").click();
@@ -33,4 +35,32 @@ Cypress.Commands.add("assertPrice", (price) => {
 
 Cypress.Commands.add("checkout", (price) => {
   cart.checkout();
+});
+
+Cypress.Commands.add("fillCustomerEmail", (email) => {
+  checkout.fillCustomerEmail(email);
+});
+
+Cypress.Commands.add("fillCustomerName", (fname, lname) => {
+  checkout.fillCustomerName(fname, lname);
+});
+
+Cypress.Commands.add("fillCustomerAddress", (address, city, postcode) => {
+  checkout.fillCustomerAddress(address, city, postcode);
+});
+
+Cypress.Commands.add("fillCustomerPhoneNo", (number) => {
+  checkout.fillCustomerPhoneNo(number);
+});
+
+Cypress.Commands.add("clickShippingContinue", () => {
+  checkout.clickShippingContinue();
+});
+
+Cypress.Commands.add("enterCardDetails", (number, expiry, name, cvv) => {
+  checkout.enterCardDetails(number, expiry, name, cvv);
+});
+
+Cypress.Commands.add("verifyOrder", (name) => {
+  checkout.verifyOrder(name);
 });
