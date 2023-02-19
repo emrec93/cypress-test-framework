f4b2e319a4c2511f36b8847733f3665b9f61d6e9
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-19 12:12:31 +0000

    add fixture data to cy.js file

--- cypress/e2e/addProductAndCheckout.cy.js
+++ cypress/e2e/addProductAndCheckout.cy.js
@@ -2,14 +2,22 @@
 
 describe("Adding a product to the cart and completing the checkout process", () => {
   it("Visits site, searches for an item, and then proceeds to checkout", () => {
-    // Home
-    cy.visit("/");
-    cy.rejectCookies();
-    cy.assertThatUserIsOnHomepage();
-    cy.clickSearch();
-    cy.searchFor("Hand Soap");
+    cy.fixture("products.json").then((products) => {
+      products.forEach((product) => {
+        // Home
+        cy.visit("/");
+        cy.rejectCookies();
+        cy.assertThatUserIsOnHomepage();
+        cy.clickSearch();
+        cy.searchFor(product.name);
 
-    // Search
-    cy.addToCart(0);
+        // Search
+        cy.addToCart(0);
+
+        // Checkout
+        cy.assertPrice(product.price);
+        cy.checkout();
+      });
+    });
   });
 });
