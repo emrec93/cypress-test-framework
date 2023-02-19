c74a43caeb7a80499d5d4d43b69ff7a442608da9
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-19 12:12:14 +0000

    add checkout details to config

--- cypress.config.js
+++ cypress.config.js
@@ -11,5 +11,15 @@ module.exports = defineConfig({
   },
   env: {
     CARD_NUMBER: "4111 1111 1111 1111",
+    CARD_NAME: "John Doe",
+    CARD_EXP: "07/29",
+    CARD_CVV: "123",
+    EMAIL: "test@test.com",
+    FIRST_NAME: "John",
+    LAST_NAME: "Doe",
+    ADDRESS: "Some Address",
+    CITY: "London",
+    POST_CODE: "SW20 ABC",
+    PHONE_NO: "07777777777",
   },
 });
