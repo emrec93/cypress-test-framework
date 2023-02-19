ea40d39f5323c1b2979c3659890d6a1c5bc48fef
Emre C <emre.cakmak@nationwide.co.uk>
2023-02-19 12:13:33 +0000

    update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,10 @@
 import Home from "../pages/Home";
 import Search from "../pages/Search";
+import Cart from "../pages/Cart";
 
 const home = new Home();
 const search = new Search();
+const cart = new Cart();
 
 Cypress.Commands.add("rejectCookies", () => {
   cy.contains("Reject all").click();
@@ -23,4 +25,12 @@ Cypress.Commands.add("searchFor", (product) => {
 
 Cypress.Commands.add("addToCart", (nth) => {
   search.addToCart(nth);
+});
+
+Cypress.Commands.add("assertPrice", (price) => {
+  cart.assertPrice(price);
+});
+
+Cypress.Commands.add("checkout", (price) => {
+  cart.checkout();
 });
